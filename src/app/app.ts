8bc2ca7a4f129cebc9f14a8044b5b22c625ed0af
Chandra Sekhar <chandu2chandu1@gmail.com>
2025-08-22 20:47:56 +0530

    Few more updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { Nav } from './components/nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
